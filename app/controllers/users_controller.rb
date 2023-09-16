da2216027e4ce94caa008d2c7991d0784f25b03d
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-09-16 13:22:11 -0400

    Make a create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    def create 
+        user = User.create(user_params)
+
+        if user.valid?
+            session[:user_id] = user.id
+
+            render json: user, status: :created
+        else
+            render json: { errors: [ "Invalid password or username" ] }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :email, :password, :password_confirmation, :image_url, :bio)
+    end
 end
