ee87f468cdbdf46d67d2827ae898378773b5cfbd
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-09-16 14:19:30 -0400

    Create components for frontend

---
+++ client/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import Login from "../pages/Login";
+
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  return (
+    <div className="App">
+      
+        
+    </div>
+  );
+}
+
+export default App;
