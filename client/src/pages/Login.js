b105a476989e2a1722444d3132fcaf9443d5db1e
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-09-16 14:49:15 -0400

    Build all components with respective fetches

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Button, Error, Input, FormField, Label } from "../styles";
 
 function LoginForm({ onLogin }) {
   const [username, setUsername] = useState("");
