29dc90b8494ef70ad71dc1198d97e9370a0e9325
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-09-16 17:51:49 -0400

    Finalize login, logout, signup

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,19 +1,20 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "./context/user";
 
-function SignUpForm({ onLogin }) {
+function SignUpForm( ) {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const [bio, setBio] = useState("");
-  const [errors, setErrors] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [errorsList, setErrorsList] = useState([]);
+  const { signup } = useContext(UserContext);
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
-    setErrors([]);
-    setIsLoading(true);
 
     fetch("/signup", {
       method: "POST",
@@ -21,85 +22,94 @@ function SignUpForm({ onLogin }) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        username,
-        email,
-        password,
+        username: username,
+        email: email,
+        password: password,
         password_confirmation: passwordConfirmation,
         image_url: imageUrl,
-        bio,
+        bio: bio
       }),
-    }).then((r) => {
-      setIsLoading(false);
-      if (r.ok) {
-        r.json().then((user) => onLogin(user));
-      } else {
-        r.json().then((err) => setErrors(err.errors));
-      }
-    });
+    })
+    .then(res => res.json())
+    .then(user => {
+        if (!user.errors) {
+            signup(user)
+            navigate('/')
+        } else {
+            setUsername("")
+            setEmail("")
+            setPassword("")
+            setPasswordConfirmation("")
+            setImageUrl("")
+            setBio("")
+            const errorLis = user.errors.map(e => <li>{e}</li>)
+            setErrorsList(errorLis)
+        }
+    })
+    
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-    
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          autoComplete="off"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+    <div>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username</label>
+            <input
+                type="text"
+                id="username"
+                autoComplete="off"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            /> <br />
 
-        <label htmlFor="email">Email Address</label>
-        <input
-          type="text"
-          id="email"
-          autoComplete="off"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+            <label htmlFor="email">Email Address</label>
+            <input
+                type="text"
+                id="email"
+                autoComplete="off"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            /> <br />
      
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          autoComplete="current-password"
-        />
+            <label htmlFor="password">Password</label>
+            <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                autoComplete="current-password"
+            /> <br />
       
-        <label htmlFor="password">Password Confirmation</label>
-        <input
-          type="password"
-          id="password_confirmation"
-          value={passwordConfirmation}
-          onChange={(e) => setPasswordConfirmation(e.target.value)}
-          autoComplete="current-password"
-        />
+            <label htmlFor="password">Password Confirmation</label>
+            <input
+                type="password"
+                id="password_confirmation"
+                value={passwordConfirmation}
+                onChange={(e) => setPasswordConfirmation(e.target.value)}
+                autoComplete="current-password"
+            /> <br />
       
-        <label htmlFor="imageUrl">Profile Image</label>
-        <input
-          type="text"
-          id="imageUrl"
-          value={imageUrl}
-          onChange={(e) => setImageUrl(e.target.value)}
-        />
-      
-        <label htmlFor="bio">Bio</label>
-        <p
-          rows="3"
-          id="bio"
-          value={bio}
-          onChange={(e) => setBio(e.target.value)}
-        />
-     
-        <button type="submit">{isLoading ? "Loading..." : "Sign Up"}</button>
-     
-        {errors.map((err) => (
-          <p key={err}>{err}</p>
-        ))}
-      
-    </form>
+            <label htmlFor="imageUrl">Profile Image</label>
+            <input
+                type="text"
+                id="imageUrl"
+                value={imageUrl}
+                onChange={(e) => setImageUrl(e.target.value)}
+            />
+
+            <label htmlFor="bio">Bio</label>
+            <input
+                type="text"
+                rows="3"
+                id="bio"
+                value={bio}
+                onChange={(e) => setBio(e.target.value)}
+            /> <br />
+            <input type="submit"></input>
+        </form>
+        <ul>
+            {errorsList}
+        </ul>
+    </div>
   );
 }
 
