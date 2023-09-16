387f2193920bc5534ae043fbea6817eaf0db88ee
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-09-16 14:24:31 -0400

    Create fetch functionality in App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,6 +7,18 @@ import Login from "../pages/Login";
 function App() {
   const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    fetch("/me")
+    .then((r) => {
+      if (r.ok) {
+        r.json()
+        .then((user) => setUser(user));
+      }
+    });
+  }, [])
+
+  if (!user) return <Login onLogin={setUser} />;
+
   return (
     <div className="App">
       
