0d103b4a94eb9ff55b1bf3be87afe0a5dd9e297f
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-09-16 21:49:43 -0400

    Include validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username, :email, :password, :password_confirmation, presence: true
+    validates :username, uniqueness: true
+    validate :password_complexity
+    validates :password, confirmation: true
     has_many :comments, dependent: :destroy 
     has_many :issues, through: :comments
+    
+    def password_complexity
+        if password.present? && !password.match(/^(?=.*[a-zA-Z])(?=.*[0-9]).{5,}$/)
+            errors.add :password, "must include at least one letter and one number and be at least 5 characters long"
+        end
+    end
+
 end
