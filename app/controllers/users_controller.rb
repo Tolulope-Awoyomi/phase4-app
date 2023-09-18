c9b64523b6240f24779177b17b3004df4d2aeaef
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-09-17 20:08:38 -0400

    Build update method in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+    before_action :one_user, only: [:show, :update, :destroy]
+    
     def create
         user = User.create(user_params)
 
@@ -20,9 +23,29 @@ class UsersController < ApplicationController
         end
     end
 
+    def update
+        if @user 
+            if @user.update!(user_params_update)
+                render json: @user, status: :accepted
+            else
+                render json: { errors: ["validation errors"]}, status: :unprocessable_entity
+            end
+        else
+            render json: { errors: ["Scientist not found"]}, status: :not_found
+        end
+    end
+
     private
 
+    def one_user
+        @user = User.find_by(id: params[:id])
+    end
+
     def user_params
+        params.permit(:username, :email, :password, :password_confirmation, :image_url, :bio)
+    end
+
+    def user_params_update
         params.permit(:username, :email, :password, :password_confirmation, :image_url, :bio)
     end
 end
