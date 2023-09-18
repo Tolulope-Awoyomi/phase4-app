171cc86ef05094aacad0c78709a0a29ace4a35a2
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-09-17 20:56:11 -0400

    Synthsize controllers

--- app/controllers/commments_controller.rb
+++ app/controllers/commments_controller.rb
@@ -1,29 +1,3 @@
 class CommmentsController < ApplicationController
-    before_action :authenticate_user!
 
-
-    def create 
-        user = User.find_by(id: params[:id])
-        user.valid?
-        comment = user.comment.create!(comment_params)
-        if comment 
-            render json: comment, status: :created
-        else
-            render json: { errors: "validation errors"}, status: :not_found
-        end
-    end
-
-    def destroy
-        user = User.find_by(id: params[:id])
-            user.comment.destroy
-            head :no_content, status: :deleted
-        else
-            render json: {error: "User not found"}, status: :not_found
-        end
-    end
-
-    private
-    def comment_params
-        params.permit(:content, :user_id, :issue_id)
-    end
 end
