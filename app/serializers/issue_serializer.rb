181b9d3770b6a613235dbf154f59462ef387e74f
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-10-12 17:57:38 -0400

    Clean up code

--- app/serializers/issue_serializer.rb
+++ app/serializers/issue_serializer.rb
@@ -3,12 +3,12 @@ class IssueSerializer < ActiveModel::Serializer
 
   def issues_with_comments
     object.comments.map { |comment| 
-    {id: comment.id,
-    content: comment.content,
-    username: comment.user.username,
-    issue_id: comment.issue_id,
-    user_id: comment.user.id
-    }
+      {id: comment.id,
+      content: comment.content,
+      username: comment.user.username,
+      issue_id: comment.issue_id,
+      user_id: comment.user.id
+      }
   }
 end
 end
